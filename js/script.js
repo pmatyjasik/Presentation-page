1d1a97ec0d7fc6d4e08321af30effe9dd3412599
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-02-22 22:13:27 +0100

    Finished page

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function openForm() {
+    document.getElementById("popup-form").style.display = "block";
+}
+function closeForm() {
+    document.getElementById("popup-form").style.display = "none";
+}
